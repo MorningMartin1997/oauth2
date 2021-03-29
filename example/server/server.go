eeaddb0719ef91d8d4c5b0103d93d91910321fdf
martinzeng <morningmartin1997@gmail.com>
2021-03-29 15:24:12 +0800

    Change pre-defined client_id and client_password

--- example/server/server.go
+++ example/server/server.go
@@ -31,8 +31,8 @@ var (
 
 func init() {
 	flag.BoolVar(&dumpvar, "d", true, "Dump requests and responses")
-	flag.StringVar(&idvar, "i", "222222", "The client id being passed in")
-	flag.StringVar(&secretvar, "s", "22222222", "The client secret being passed in")
+	flag.StringVar(&idvar, "i", "111111", "The client id being passed in")
+	flag.StringVar(&secretvar, "s", "111111", "The client secret being passed in")
 	flag.StringVar(&domainvar, "r", "http://localhost:9094", "The domain of the redirect url")
 	flag.IntVar(&portvar, "p", 9096, "the base port for the server")
 }
@@ -143,7 +143,7 @@ func main() {
 	log.Printf("Server is running at %d port.\n", portvar)
 	log.Printf("Point your OAuth client Auth endpoint to %s:%d%s", "http://localhost", portvar, "/oauth/authorize")
 	log.Printf("Point your OAuth client Token endpoint to %s:%d%s", "http://localhost", portvar, "/oauth/token")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",portvar), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portvar), nil))
 }
 
 func dumpRequest(writer io.Writer, header string, r *http.Request) error {
